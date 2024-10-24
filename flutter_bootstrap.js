5cd75fc944b81755088ab62ae6491d36c4a6de60
Alok Kuchlous <alokk@mirafra.com>
2024-10-24 17:47:42 +0530

    Audio

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "825506506"
+    serviceWorkerVersion: "3447730424"
   }
 });
